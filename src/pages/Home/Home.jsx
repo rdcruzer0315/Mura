d41d4c69cc8ffe618fcbbac62740794bfda97200
smart-dev <vivitedyx99@gmail.com>
2023-06-06 15:38:46 -0400

    change cafe time

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -36,7 +36,6 @@ const Home = () => {
             <div className="w-1/2 flex flex-col justify-center items-center">
                 <p className="font-bold text-black lg:text-5xl text-lg text-center">CAFE MENU</p>
                 <p className="font-semibold text-black lg:text-xl text-xs text-center lg:mt-6 mt-3">Open - 7 PM</p>
-                <p className="font-semibold text-black lg:text-xl text-xs text-center lg:mt-6 mt-3">5 PM - Close</p>
                 <NavLink to="/cafe" className="flex justify-center">
                     <img className="w-1/2 lg:mt-6 mt-3 cursor-pointer" src="assets/see_menu.png" alt="cafe_logo"/>
                 </NavLink>
