ee128866977706f425653e818ef68329e5f0c9d7
smart-dev <vivitedyx99@gmail.com>
2023-06-08 12:07:43 -0400

    footer change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,25 +9,25 @@ const Footer = () => {
         </div>
         <div className="mt-10 pb-20 grid lg:grid-cols-3 grid-cols-1">
             <div className="lg:col-start-1 lg:col-span-1 w-full">
-                <p className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">CONTACT</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">CONTACT</p>
                 <div className="flex flex-row items-center justify-center lg:mt-8 mt-3 cursor-pointer">
                     <img className="lg:w-6 w-4" src="assets/phone.png" alt="phone"/>
-                    <p className="font-bold text-[#F5ECE6] ml-4 lg:text-xl text-xs text-center tracking-widest">+1 _______________</p>
+                    <p style={{ fontFamily:"Gillsans" }} className="font-bold text-[#F5ECE6] ml-4 lg:text-xl text-xs text-center tracking-widest">+1 _______________</p>
                 </div>
                 <div className="flex flex-row items-center justify-center cursor-pointer lg:mt-3 mt-1">
                     <img className="lg:w-6 w-4" src="assets/email.png" alt="email"/>
-                    <p className="font-bold text-black ml-4 lg:text-xl text-xs text-center tracking-widest">INFO@MURAMB.COM</p>
+                    <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black ml-4 lg:text-xl text-xs text-center tracking-widest">INFO@MURAMB.COM</p>
                 </div>
             </div>
             <div className="lg:col-start-2 lg:col-span-1 w-full lg:mt-0 mt-10">
-                <p className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">HOURS OF OPERATION</p>
-                <p className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">Monday - Wednesday</p>
-                <p className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">12 PM - 10 PM</p>
-                <p className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">Thursday to Sunday</p>
-                <p className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">11 AM - 11 PM</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">HOURS OF OPERATION</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">Monday - Wednesday</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">12 PM - 10 PM</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">Thursday to Sunday</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-xl text-xs text-center tracking-widest">11 AM - 11 PM</p>
             </div>
             <div className="lg:col-start-3 lg:col-span-1 w-full lg:mt-0 mt-10">
-                <p className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">CONNECT WITH US</p>
+                <p style={{ fontFamily:"Gillsans" }} className="font-bold text-black lg:text-2xl text-lg text-center tracking-widest">CONNECT WITH US</p>
                 <div className="flex flex-row justify-center lg:mt-8 mt-3">
                     <a href="https://instagram.com/mura_myrtlebeach?igshid=MzRlODBiNWFlZA==" target="_blank"><img className="cursor-pointer lg:w-8 lg:h-8 w-5 h-5" src="assets/Home/instagram.png" alt="instagram" /></a>
                     <a href="https://www.facebook.com/profile.php?id=100093174541363&mibextid=ZbWKwL" target="_blank"><img className="cursor-pointer lg:w-8 lg:h-8 w-5 h-5 ml-5" src="assets/Home/social.png" alt="facebook" /></a>
